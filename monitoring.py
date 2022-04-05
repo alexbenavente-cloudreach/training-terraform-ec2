1c5bdeeff1d5709be68cf1dc2864a583a641341e
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-04-05 16:26:43 -0400

    Adding GH Actions terraform plan and approve

--- monitoring.py
+++ monitoring.py
@@ -4,10 +4,10 @@ html_page = "<html><body><h1>List of Private AWS Instances</h1>"
 
 # GET SUBNET INFORMATION
 def getPrivateSubnetId():
-    ec2_client = boto3.client('ec2', region_name= "us-east-1")
+    ec2_client = boto3.client("ec2", region_name="us-east-1")
     subnets_info = ec2_client.describe_subnets()
 
-    for subnet in subnets_info['Subnets']:
+    for subnet in subnets_info["Subnets"]:
         if subnet["Tags"][0]["Value"] == "talent-academy-private-a":
             private_subnet = subnet["SubnetId"]
             return private_subnet
@@ -15,17 +15,20 @@ def getPrivateSubnetId():
 
 # LIST INSTANCE INFORMATION
 private_subnet = getPrivateSubnetId()
-ec2 = boto3.resource('ec2', region_name = "us-east-1")
-    
+ec2 = boto3.resource("ec2", region_name="us-east-1")
+
 for instance in ec2.instances.all():
     if instance.subnet_id == private_subnet:
         html_page += "<p>Id: {0}<br>Type: {1}<br>AMI: {2}<br>State: {3}<br>Subnet: {4}</p>".format(
-            instance.id, instance.instance_type, instance.image.id, instance.state, instance.subnet_id
-            )
+            instance.id,
+            instance.instance_type,
+            instance.image.id,
+            instance.state,
+            instance.subnet_id,
+        )
 
 html_page += "</body></html>"
 
 html_file = open("index.html", "w")
 html_file.write(html_page)
 html_file.close()
-
