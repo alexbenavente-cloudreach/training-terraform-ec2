beb7f7cc36f70cb3fd04b12b1acfb95332c498f3
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-04-05 10:58:22 -0400

    ran terraform format command

--- security-groups.tf
+++ security-groups.tf
@@ -13,12 +13,12 @@ resource "aws_security_group" "my_public_app_sg" {
   }
 
   ingress {
-      description = "Allow HTTP traffic"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["108.196.163.36/32"]
-    }
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["108.196.163.36/32"]
+  }
 
   # OUTBOUND CONNECTIONS
   egress {
